76446c9b5d2b8eaa42b1227456ae018097bbbdf1
HARIKISHAN98 <harikishanmittal98@gmail.com>
2026-08-29 02:21:47 +0530

    feat(backend): setup project structure, database connection, and gitignore

---
+++ backend/app/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_URL = "sqlite:///./service_desk.db"
+
+engine = create_engine( DATABASE_URL, connect_args={"check_same_thread":False})
+
+sessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+
+
